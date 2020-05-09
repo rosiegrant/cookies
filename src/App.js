24577304a80897f81970a7b98c07cf5bbb2cc99b
rosegrant <rosellen.grant@gmail.com>
2020-05-09 14:07:35 -0400

    restart button and additional ingredients

--- src/App.js
+++ src/App.js
@@ -4,6 +4,86 @@ import bear from './bear.png'
 import './App.css';
 import  CheckBox  from './CheckBox.js';
 
+const starterCardData = [
+  {
+    index: 0,
+    title: "First, you pick the oats",
+    text: "Oats",
+    options: [ 
+      {
+        value: "Regular Oats",
+        isChecked: false
+      },
+      {
+        value:"Quick Oats",
+        isChecked: false
+      },
+      {
+        value:"Steel Cut Oats",
+        isChecked: false
+      }      
+    ]
+  },
+  {
+    index: 1,
+    title: "Second, you pick the flour",
+    text: "Flour",
+    options: [ 
+      {
+        value: "Almond Flour",
+        isChecked: false
+      },
+      {
+        value:"Whole Wheat Flour",
+        isChecked: false
+      },
+      {
+        value:"Pastry Flour",
+        isChecked: false
+      }      
+    ]
+  },
+  {
+    index: 2,
+    title: "Next, you pick the goodies",
+    text: "Goodies",
+    options: [ 
+      {
+        value: "Raisins",
+        isChecked: false
+      },
+      {
+        value:"Dark Chocolate",
+        isChecked: false
+      },
+      {
+        value:"Milk Chocolate",
+        isChecked: false
+      },      
+    ]
+  },
+  {
+    index: 3,
+    title: "Finally, how sweet do you like your cookies",
+    text: "Sugar",
+    options: [ 
+      {
+        value: "Very sweet",
+        isChecked: false
+      },
+      {
+        value:"Mildly sweet",
+        isChecked: false
+      },
+      {
+        value:"On the not-sweet side",
+        isChecked: false
+      },      
+    ]
+  }
+
+]
+
 class Card extends React.Component {
   
   constructor(props) {
@@ -11,8 +91,40 @@ class Card extends React.Component {
     this.handleNextClick = this.handleNextClick.bind(this);
     this.handleCheck = this.handleCheck.bind(this);
     this.createRecipe = this.createRecipe.bind(this);
+    this.startOver = this.startOver.bind(this);
     this.state = {
       currentCard: 0,
+      cardData: starterCardData,
+      lastCard: false
+    }
+
+  }
+
+  createRecipe() {
+
+    const ingredients = this.state.cardData.map((card) => {
+      // console.log(card);
+      return (
+        card.options.map((option) => {
+          if (option.isChecked) {
+            return <li class="list-group-item" key={option.value}>{option.value}</li>
+          }
+        }
+        )
+      )
+    })
+    return (
+      <ul class="list-group list-group flush">{ingredients}</ul>
+       
+          
+              
+    )
+  }
+
+  startOver(props) {
+    this.setState ({
+      currentCard: 0,
+      //there has to be a better way to do this, look into immutables
       cardData: [
         {
           index: 0,
@@ -51,32 +163,49 @@ class Card extends React.Component {
               isChecked: false
             }      
           ]
+        },
+        {
+          index: 2,
+          title: "Next, you pick the goodies",
+          text: "Goodies",
+          options: [ 
+            {
+              value: "Raisins",
+              isChecked: false
+            },
+            {
+              value:"Dark Chocolate",
+              isChecked: false
+            },
+            {
+              value:"Milk Chocolate",
+              isChecked: false
+            },      
+          ]
+        },
+        {
+          index: 3,
+          title: "Finally, how sweet do you like your cookies",
+          text: "Sugar",
+          options: [ 
+            {
+              value: "Very sweet",
+              isChecked: false
+            },
+            {
+              value:"Mildly sweet",
+              isChecked: false
+            },
+            {
+              value:"On the not-sweet side",
+              isChecked: false
+            },      
+          ]
         }
       ],
       lastCard: false
-    }
-
-  }
-
-  createRecipe() {
-
-    const ingredients = this.state.cardData.map((card) => {
-      // console.log(card);
-      return (
-        card.options.map((option) => {
-          if (option.isChecked) {
-            return <li class="list-group-item" key={option.value}>{option.value}</li>
-          }
-        }
-        )
-      )
     })
-    return (
-      <ul class="list-group list-group flush">{ingredients}</ul>
-       
-          
-              
-    )
+    console.log(starterCardData);
   }
 
   handleNextClick(props) {
@@ -117,6 +246,7 @@ class Card extends React.Component {
     const currentCardDataOptions = currentCardData.options;
     if (this.state.lastCard) {
       return (
+        <div>
       <div class="row justify-content-center">
         <div class="col-sm-6"> 
           <div class="card text-center survey-card">
@@ -127,6 +257,10 @@ class Card extends React.Component {
           </div>
         </div>
       </div>
+            <div class="row justify-content-center">
+            <RestartButton onClick={this.startOver}/>
+            </div>
+            </div>
       )
     }
     return (
@@ -146,10 +280,11 @@ class Card extends React.Component {
               </div>
           </div>
         </div>
-      </div>
-      <div class="row justify-content-center">
-      <NextButton onClick={this.handleNextClick}/>
-      </div>
+        </div>
+        <div class="row justify-content-center">
+        <NextButton onClick={this.handleNextClick}/>
+        </div>
+
       </div>
     )
   }
@@ -160,6 +295,13 @@ class Card extends React.Component {
 function NextButton(props) {
   return (
     <button type="button" class="btn btn-outline-primary" onClick={props.onClick}>Next</button>
+  )
+}
+
+function RestartButton(props) {
+  return (
+    <button type="button" class="btn btn-outline-secondary" onClick={props.onClick}>Restart</button>
+
   )
 }
 
